0bafe0a109a0907f2e9e1a5b6cf220ea24e692ab
Flysociety <Flysociety@Flysocietys-MacBook-Pro.local>
2014-09-02 16:45:46 -0500

    debugging and formatting applied

--- high_low.php
+++ high_low.php
@@ -1,38 +1,60 @@
 <?php
 
-$answer = mt_rand(1,100);
+// High Low Game!
 
-$number_of_guesses = 0;
+// Welcome to the world of game development!
 
-fwrite (STDOUT, "Welcome to the HIGHER OR LOWER game.\n");
+// You are going to build a game using the CLI.
 
-do{
-	fwrite (STDOUT, "Guess a number.  ");
+// The specs for the game are:
 
-	$guess = trim(fgets(STDIN));
+// game picks a random number between 1 and 100.
+// prompts user to guess the number
+// if user's guess is less than the number, it outputs "HIGHER"
+// if user's guess is more than the number, it outputs "LOWER"
+// if a user guesses the number, the game should declare "GOOD GUESS!"
 
-	$number_of_guesses++;
 
-	if($guess== $answer){
-	fwrite (STDOUT, "GOOD GUESS! HIP HIP HOORAY!!!! Rock on.\n");
+
+	if ($argc == 3) {
+		$min = $argv[1];
+		$max = $argv[2];
+
+		$answer = rand($min, $max);
 	}
+
+	else ($answer = rand(1,100));
+
+	$number_of_guesses = 0;
+
+	fwrite (STDOUT, "Welcome to the HIGHER OR LOWER game.".PHP_EOL);
+
+	do{
+		fwrite (STDOUT, "Guess a number.  ");
+
+		$guess = trim(fgets(STDIN));
+
+		$number_of_guesses++;
+
+			if($guess== $answer){
+			fwrite (STDOUT, "GOOD GUESS! HIP HIP HOORAY!!!! Rock on.".PHP_EOL);
+			}
 	
-	elseif ($guess > $answer){
-	fwrite(STDOUT, "$guess is too HIGH. guess again.  \n");
+			elseif ($guess > $answer){
+			fwrite(STDOUT, "$guess is too HIGH. guess again.  ".PHP_EOL);
+			}
+
+			else{
+			fwrite (STDOUT, "$guess is too LOW. guess again  ".PHP_EOL);
+			}
 	}
+	while ($guess != $answer);
 
-	else{
-	fwrite (STDOUT, "$guess is too LOW. guess again  \n");
-	}
-}
+	usleep (500000);
 
-while ($guess != $answer);
-
-	if($guess > 1){
-	fwrite(STDOUT, "It took you $number_of_guesses guesses to figure it out.  \n");
-	}
-
-	else (fwrite(STDOUT, "It took you $number_of_guesses guess to figure it out.  \n"));
+	fwrite(STDOUT, "It took you $number_of_guesses guesses to figure it out.  ".PHP_EOL);
+			
 	
+
 
 	?>
