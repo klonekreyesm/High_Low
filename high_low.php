f4ddf9eb7c3f211a7745770ef44f0ea8f809bc2b
Flysociety <Flysociety@Flysocietys-MacBook-Pro.local>
2014-09-12 09:44:19 -0500

    Changes made to README.md to reflect game instructions.

--- high_low.php
+++ high_low.php
@@ -1,18 +1,5 @@
 <?php
 
-// High Low Game!
-
-// Welcome to the world of game development!
-
-// You are going to build a game using the CLI.
-
-// The specs for the game are:
-
-// game picks a random number between 1 and 100.
-// prompts user to guess the number
-// if user's guess is less than the number, it outputs "HIGHER"
-// if user's guess is more than the number, it outputs "LOWER"
-// if a user guesses the number, the game should declare "GOOD GUESS!"
 
 if ($argc == 3) {
     $min = $argv[1];
